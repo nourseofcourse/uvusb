4d15fedc2a9c33d113d10a0f42f9357495e21c2c
Brandon Nourse <nourseofcourse@gmail.com>
2020-05-28 17:32:27 -0400

    adding a section + magnific popup package

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
 import $ from 'jquery';
 import 'slick-carousel';
+import Headroom from 'headroom.js';
+
 
 $(document).ready(() => {
+
+  const myHeader = document.querySelector('header.headroom');
+
+  let headroom = new Headroom(myHeader);
+
+  headroom.init();
+
   $('.js-testimonial-slider').slick({
     dots: true,
     infinite: true,
